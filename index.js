cbee7a3c947e70e66afd095cdb25789d6614ec00
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 18:37:43 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 function init() {
-  //put any page initialization/handlebars initialization here
+  handlebarsSetup()
+  var formTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(formTemplate)
+  document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
 document.addEventListener("DOMContentLoaded", function(event) {
   init()
