ee75376c7e4d1a0c517f7c11a723a4219c57bd56
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 18:38:28 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ function init() {
   var template = Handlebars.compile(formTemplate)
   document.getElementsByTagName("main")[0].innerHTML = template({'submitAction': 'createRecipe()'})
 }
-document.addEventListener("DOMContentLoaded", function(event) {
-  init()
-})
+
+function createRecipe() {
+  var recipe = recipeValues()
+  var recipeTemplate = document.getElementById("recipe-template").innerHTML
+  var template = Handlebars.compile(recipeTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
