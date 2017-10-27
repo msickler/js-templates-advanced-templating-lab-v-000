c92a1ceff80eb5da869ccb51e7687d6157679b08
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-27 18:39:36 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,3 +18,17 @@ function updateRecipe() {
   var template = Handlebars.compile(recipeTemplate)
   document.getElementById("main").innerHTML = template(recipe)
 }
+
+function displayEditForm() {
+  var name = document.getElementById("nameHeader").innerText
+  var description = document.getElementById("recipeDescription").innerText
+  var ingredientsList = document.getElementsByName("ingredientsList")
+  var ingredients = []
+  for(var i=0;i<ingredientsList.length;i++) {
+    ingredients.push(ingredientsList[i].innerText)
+  }
+  var recipe = {name, description, ingredients, submitAction: 'createRecipe()'}
+  var recipeFormTemplate = document.getElementById("recipe-form-template").innerHTML
+  var template = Handlebars.compile(recipeFormTemplate)
+  document.getElementById("main").innerHTML = template(recipe)
+}
